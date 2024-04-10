54c7a209cafd0d01010eb869d40d408fe2dffa09
Vlad <gladladvlad@gmail.com>
2024-04-10 20:19:43 +0300

    basic testing: reading some json and download first result

--- dl-playlist.py
+++ dl-playlist.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
 import time
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -9,7 +10,24 @@ import yt_dlp
 
 #time.sleep(2)
 
-link = "https://www.youtube.com/results?search_query={}".format("chris+hazelton+bambu")
+plist_file = open('spotify-playlists.json', 'r')
+playlists = json.loads(plist_file.read())
+plist_file.close()
+
+for plist in playlists:
+    print('---')
+    print("\t" + plist['name'])
+    print('---')
+
+    for track in plist['tracks']:
+        artist = track['artist']
+        name = track['name']
+        album = track['album']
+        print(f'track: "{name}" by "{artist}" from "{album}"')
+
+#TODO: sanitize artist name, track name, etc.
+query = f"{playlists[0]['tracks'][0]['artist'].replace(' ', '+')}+{playlists[0]['tracks'][0]['name'].replace(' ', '+')}"
+link = "https://www.youtube.com/results?search_query={}".format(query)
 
 webdriver_options = Options()
 webdriver_options.add_argument("--headless")
@@ -26,9 +44,10 @@ for item in search_results:
     print("link: " + item.get_attribute("href"))
 
 chosen = search_results[0].get_attribute('href')
+#TODO: add indexes to beginning of filename so that the order stays the same on the fs
 ytdl_options = {'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'}
 
 ytdl = yt_dlp.YoutubeDL(ytdl_options)
 ytdl.download(chosen)
 
-driver.quit();
+driver.quit()
