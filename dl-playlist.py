1595006050ef394af266d0d56417928eb62b5bea
Vlad <gladladvlad@gmail.com>
2024-04-12 13:04:37 +0300

    search yt for artist + track name for each track in a playlist

--- dl-playlist.py
+++ dl-playlist.py
@@ -8,46 +8,92 @@ from selenium.webdriver.chrome.options import Options
 
 import yt_dlp
 
-#time.sleep(2)
+min_wait = 7.5
 
-plist_file = open('spotify-playlists.json', 'r')
-playlists = json.loads(plist_file.read())
-plist_file.close()
+def load_playlists():
+    plist_file = open('spotify-playlists.json', 'r')
+    playlists = json.loads(plist_file.read())
+    plist_file.close()
 
-for plist in playlists:
-    print('---')
-    print("\t" + plist['name'])
-    print('---')
+    return playlists
 
-    for track in plist['tracks']:
-        artist = track['artist']
-        name = track['name']
-        album = track['album']
-        print(f'track: "{name}" by "{artist}" from "{album}"')
 
-#TODO: sanitize artist name, track name, etc.
-query = f"{playlists[0]['tracks'][0]['artist'].replace(' ', '+')}+{playlists[0]['tracks'][0]['name'].replace(' ', '+')}"
-link = "https://www.youtube.com/results?search_query={}".format(query)
+def list_playlists(playlists):
+    index = 0
+    for plist in playlists:
+        #print('---')
+        print(str(index) + " " + plist['name'])
+        #print('---')
 
-webdriver_options = Options()
-webdriver_options.add_argument("--headless")
-webdriver_options.add_argument("--disable-crash-reporter")
+       #for track in plist['tracks']:
+       #    artist = track['artist']
+       #    name = track['name']
+       #    album = track['album']
+       #    print(f'track: "{name}" by "{artist}" from "{album}"')
 
-driver = webdriver.Chrome(options=webdriver_options)
-driver.implicitly_wait(0.5)
-driver.get(link)
+        index += 1
 
-search_results = driver.find_elements(by=By.CSS_SELECTOR, value="a[id='video-title']")
 
-for item in search_results:
-    print("details: " + item.get_attribute("aria-label"))
-    print("link: " + item.get_attribute("href"))
+def start_webdriver():
+    webdriver_options = Options()
+    webdriver_options.add_argument("--headless")
+    webdriver_options.add_argument("--disable-crash-reporter")
 
-chosen = search_results[0].get_attribute('href')
-#TODO: add indexes to beginning of filename so that the order stays the same on the fs
-ytdl_options = {'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'}
+    driver = webdriver.Chrome(options=webdriver_options)
+    driver.implicitly_wait(0.5)
 
-ytdl = yt_dlp.YoutubeDL(ytdl_options)
-ytdl.download(chosen)
+    return driver
+
+
+def search_youtube(driver, query):
+    #TODO: sanitize artist name, track name, etc.
+    query = query.replace(" ", "+")
+    link = "https://www.youtube.com/results?search_query={}".format(query)
+    driver.get(link)
+    search_results = driver.find_elements(by=By.CSS_SELECTOR, value="a[id='video-title']")
+
+    return search_results
+
+
+def dl_track(driver, track, ytdl_options):
+    query = f"{track['artist']} {track['name']}"
+    search_results = search_youtube(driver, query)
+    chosen = search_results[0].get_attribute('href')
+
+    ytdl = yt_dlp.YoutubeDL(ytdl_options)
+    ytdl.download(chosen)
+
+
+def dl_playlist(driver, playlist):
+    track_index = 0
+    for track in playlist['tracks']:
+        filename_prefix = str(track_index).zfill(len(str(len(playlist['tracks']))))
+
+        before = time.time()
+        ytdl_options = {'extract_audio': True, 'format': 'bestaudio', 'outtmpl': f'{filename_prefix} - %(title)s.mp3'}
+        dl_track(driver, track, ytdl_options)
+        after = time.time()
+        elapsed = after - before
+
+        if elapsed < min_wait:
+            time.sleep(min_wait - elapsed)
+
+        track_index += 1
+
+
+playlists = load_playlists()
+list_playlists(playlists)
+
+print("Which playlist to download? Input the index: ")
+plist_index = int(input())
+
+driver = start_webdriver()
+
+playlist = playlists[plist_index]
+dl_playlist(driver, playlist)
+
+#for item in search_results:
+#    print("details: " + item.get_attribute("aria-label"))
+#    print("link: " + item.get_attribute("href"))
 
 driver.quit()
